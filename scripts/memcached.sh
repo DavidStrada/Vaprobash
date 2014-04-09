f7bdd187f0433eeec8431fb80f327d904ea0b5ca
David <idavid.str@gmail.com>
2014-04-09 13:49:40 -0600

    memcache file configured

--- scripts/memcached.sh
+++ scripts/memcached.sh
@@ -2,3 +2,9 @@
 
 # Install Memcached
 sudo apt-get install -y memcached
+sudo apt-get install -y php5-memcache
+sudo apt-get install php-pear
+sudo apt-get install build-essential
+sudo pecl install memcache
+echo "extension=memcache.so" | sudo tee /etc/php5/conf.d/memcache.ini
+ps aux | grep memcache
