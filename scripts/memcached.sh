32116d8819c2995d42ef6ba6a952df702862efae
David <idavid.str@gmail.com>
2014-04-10 00:17:48 -0600

    fixed -y on memcached file

--- scripts/memcached.sh
+++ scripts/memcached.sh
@@ -3,8 +3,8 @@
 # Install Memcached
 sudo apt-get install -y memcached
 sudo apt-get install -y php5-memcache
-sudo apt-get install php-pear
-sudo apt-get install build-essential
+sudo apt-get install -y php-pear
+sudo apt-get install -y build-essential
 sudo pecl install memcache
 echo "extension=memcache.so" | sudo tee /etc/php5/conf.d/memcache.ini
 ps aux | grep memcache
