c3ecea8288f60ada35479d18ee758664382764be
David <idavid.str@gmail.com>
2014-04-10 21:25:16 -0600

    fixed php5-fig error on console

--- scripts/apache.sh
+++ scripts/apache.sh
@@ -32,22 +32,5 @@ sudo mv vhost /usr/local/bin
 # Create a virtualhost to start, with SSL certificate
 sudo vhost -s $1.xip.io -d $public_folder -p /etc/ssl/xip.io -c xip.io
 
-if [[ $PHP_IS_INSTALLED -eq 0 ]]; then
-    # PHP Config for Apache
-    cat > /etc/apache2/conf-available/php5-fpm.conf << EOF
-    <IfModule mod_fastcgi.c>
-            AddHandler php5-fcgi .php
-            Action php5-fcgi /php5-fcgi
-            Alias /php5-fcgi /usr/lib/cgi-bin/php5-fcgi
-            FastCgiExternalServer /usr/lib/cgi-bin/php5-fcgi -socket /var/run/php5-fpm.sock -pass-header Authorization
-            <Directory /usr/lib/cgi-bin>
-                    Options ExecCGI FollowSymLinks
-                    SetHandler fastcgi-script
-                    Require all granted
-            </Directory>
-    </IfModule>
-EOF
-    sudo a2enconf php5-fpm
-fi
 
 sudo service apache2 restart
