b38604c39b3ec667d866df4f39fd8d76009bc474
David <idavid.str@gmail.com>
2014-04-10 21:37:46 -0600

    remove erros on memcached

--- scripts/memcached.sh
+++ scripts/memcached.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
 # Install Memcached
-
-sudo apt-get install -y php-pear
-sudo apt-get install -y build-essential
-sudo pecl install memcache
-echo "extension=memcache.so" | sudo tee /etc/php5/conf.d/memcache.ini
-ps aux | grep memcache
