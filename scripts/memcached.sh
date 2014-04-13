0426910de34d4bbfff36cee4b7da672aacff716c
David <idavid.str@gmail.com>
2014-04-13 14:45:58 -0600

    fixed memchached

--- scripts/memcached.sh
+++ scripts/memcached.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
 # Install Memcached
+sudo apt-get install -y php5-memcache memcached php5-dev
+sudo pecl install memcache
+
+echo "extension=memcache.so" | sudo tee /etc/php5/conf.d/memcache.ini
